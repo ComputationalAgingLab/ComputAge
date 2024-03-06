d79a96c74b21a1b4bf4f1fe19d7451ce87cdcacb
GentlemanOfFate <gentlemanoffate@gmail.com>
2024-03-06 14:01:02 +0300

    Added function to load datasets descriptions and a separate worker function to download a file

--- computage/data_library/data_repository.py
+++ computage/data_library/data_repository.py
@@ -7,51 +7,100 @@ import requests
 import pandas as pd
 from tqdm.notebook import trange
 
-def load_data(url: str,
-              out_dir: str,
-              dataset_id: str,
-              force = False) -> tuple[pd.DataFrame, pd.DataFrame]:
+URL = 'http://localhost:9998/'
+OUT_DIR = 'datasets'
+
+import os
+import requests
+import pandas as pd
+from tqdm.notebook import trange
+
+URL = 'http://localhost:9998/'
+DATA_DIR = 'datasets'
+
+def download_file(url: str, out_path: str) -> None:
     
     """
-    Downloads data from a remote repository. Downloaded file is saved to out_dir/file_name
+    Worker function to download a file
 
     Parameters:
-        url (str): Repository web link
-        out_dir (str): Output directiory containing downloaded data
+        url (str): Remote repository link
+        out_path (str): Output file path
+    """
+    
+    # Streaming, so we can iterate over the response
+    response = requests.get(url, stream=True)
+
+    # Sizes in bytes
+    total_size = int(response.headers.get('content-length', 0))
+    block_size = 1024
+
+    # Download file if it doesn't exist in the output directory
+    if not os.path.exists(out_path) or total_size != os.path.getsize(out_path):
+        with trange(1, total=total_size, desc=f'Loading {out_path.split("/")[-1]}', unit='B', unit_scale=True) as progress_bar:
+            if '.pkl' in out_path: 
+                mode = 'wb'
+            else:
+                mode = 'w'
+            with open(out_path, mode) as f:
+                for downloaded in response.iter_content(block_size):
+                    progress_bar.update(len(downloaded))
+                    f.write(downloaded)
+        if total_size != 0 and progress_bar.n != total_size:
+            raise RuntimeError('Failed to download file. Please check if it exists in the remote repository.')
+    
+    return
+
+def load_data(dataset_id: str,
+              data_dir=DATA_DIR) -> tuple[pd.DataFrame, pd.DataFrame]:
+    
+    """
+    Downloads data from a remote repository. Downloaded file is saved to data_dir/file_name
+
+    Parameters:
         dataset_id (str): Dataset ID
-        force (bin): If True, enables overwriting (currently unused)
+        data_dir (str): Local directiory containing downloaded data
 
     Returns:
         tuple(data, meta)
     """
 
     file_name = f'{dataset_id}.pkl.xz'
-
-    full_url = f'{url}/{file_name}'
-
-    # Streaming, so we can iterate over the response
-    response = requests.get(full_url, stream=True)
-
-    # Sizes in bytes
-    total_size = int(response.headers.get('content-length', 0))
-    block_size = 1024
+    file_url = f'{URL}/{file_name}'
 
     # Create output directory if it doesn't exist
-    if not os.path.exists(out_dir): os.makedirs(out_dir)
+    if not os.path.exists(data_dir): os.makedirs(data_dir)
 
     # Output file path
-    out_path = f'{out_dir}/{file_name}'
+    out_path = f'{data_dir}/{file_name}'
 
-    # Download file if it doesn't exist in the output directory
-    if not (os.path.exists(out_path) and total_size == os.path.getsize(out_path)):
-        with trange(1, total=total_size, desc=f'Loading {dataset_id}', unit='B', unit_scale=True) as progress_bar:
-            with open(out_path, 'wb') as f:
-                for downloaded in response.iter_content(block_size):
-                    progress_bar.update(len(downloaded))
-                    f.write(downloaded)
-        if total_size != 0 and progress_bar.n != total_size:
-            raise RuntimeError('Failed to download file. Please check if it exists in the remote repository.')
-        
+    # Function to download file
+    download_file(out_path=out_path, url=file_url)
+    
+    # Read downloaded pickled dictionary into two data frames
     data, meta = pd.read_pickle(out_path).values()
 
     return data, meta
+
+def describe_data(data_dir=DATA_DIR) -> pd.DataFrame:
+
+    """
+    Downloads description for all datasets contained in a remote repository. Downloaded file is saved to data_dir/datasets_description.csv
+
+    Parameters:
+        data_dir (str): Local directiory containing downloaded data
+
+    Returns:
+        pd.DataFrame with annotated datasets
+    """
+
+    file_name = 'datasets_description.csv'
+    file_url = f'{URL}/{file_name}'
+
+    if not os.path.exists(data_dir): os.makedirs(data_dir)
+    out_path = f'{data_dir}/{file_name}'
+    download_file(out_path=out_path, url=file_url)
+        
+    data_descr = pd.read_csv(out_path, index_col=0)
+
+    return data_descr
