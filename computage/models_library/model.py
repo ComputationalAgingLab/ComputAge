1880944ef0f5f2611718112e06ebbf3e0eb36d86
shappiron <fire-paladin@mail.ru>
2024-04-25 10:07:17 +0300

    added clock models and non finished version of model builder

---
+++ computage/models_library/model.py
@@ -0,0 +1,418 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# from biolearn.dunedin_pace import dunedin_pace_normalization
+from computage.deage.base import PublishedClocksBaseEstimator
+from computage.utils.nan_utils import impute_from_average, impute_from_standard
+from computage.settings import ROOTDIR
+import os
+
+#function for clock file retrieval
+def get_clock_file(filename):
+    clock_file_path = os.path.join(ROOTDIR, "models_library/clocks", filename)  
+    return clock_file_path
+
+#horvath-specific ELU-like transform
+def anti_trafo(x, adult_age=20):
+    y = np.where(
+        x < 0, (1 + adult_age) * np.exp(x) - 1, (1 + adult_age) * x + adult_age
+    )
+    return y
+
+#identity transform if not given other
+def identity(x):
+    return x
+
+
+model_definitions = {
+    "Horvathv1": {
+        "year": 2013,
+        "species": "Human",
+        "tissue": "Multi-tissue",
+        "source": "https://genomebiology.biomedcentral.com/articles/10.1186/gb-2013-14-10-r115",
+        "output": "Age (Years)",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "Horvath1.csv",
+            "transform": lambda sum: anti_trafo(sum + 0.696),
+        },
+    },
+    "Hannum": {
+        "year": 2013,
+        "species": "Human",
+        "tissue": "Blood",
+        "source": "https://www.sciencedirect.com/science/article/pii/S1097276512008933",
+        "output": "Age (Years)",
+        "model": {"type": "LinearMethylationModel", "file": "Hannum.csv"},
+    },
+    "Lin": {
+        "year": 2016,
+        "species": "Human",
+        "tissue": "Blood",
+        "source": "https://www.aging-us.com/article/100908/text",
+        "output": "Age (Years)",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "Lin.csv",
+            "transform": lambda sum: sum + 12.2169841,
+        },
+    },
+    "PhenoAge": {
+        "year": 2018,
+        "species": "Human",
+        "tissue": "Blood",
+        "source": "https://www.aging-us.com/article/101414/text",
+        "output": "Age (Years)",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "PhenoAge.csv",
+            "transform": lambda sum: sum + 60.664,
+        },
+    },
+    "YingCausAge": {
+        "year": 2022,
+        "species": "Human",
+        "tissue": "Blood",
+        "source": "https://www.biorxiv.org/content/10.1101/2022.10.07.511382v2",
+        "output": "Age (Years)",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "YingCausAge.csv",
+            "transform": lambda sum: sum + 86.80816381,
+        },
+    },
+    "YingDamAge": {
+        "year": 2022,
+        "species": "Human",
+        "tissue": "Blood",
+        "source": "https://www.biorxiv.org/content/10.1101/2022.10.07.511382v2",
+        "output": "Age (Years)",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "YingDamAge.csv",
+            "transform": lambda sum: sum + 543.4315887,
+        },
+    },
+    "YingAdaptAge": {
+        "year": 2022,
+        "species": "Human",
+        "tissue": "Blood",
+        "source": "https://www.biorxiv.org/content/10.1101/2022.10.07.511382v2",
+        "output": "Age (Years)",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "YingAdaptAge.csv",
+            "transform": lambda sum: sum - 511.9742762,
+        },
+    },
+    "Horvathv2": {
+        "year": 2018,
+        "species": "Human",
+        "tissue": "Skin + blood",
+        "source": "https://www.aging-us.com/article/101508/text",
+        "output": "Age (Years)",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "Horvath2.csv",
+            "transform": lambda sum: anti_trafo(sum - 0.447119319),
+        },
+    },
+    "PEDBE": {
+        "year": 2019,
+        "species": "Human",
+        "tissue": "Buccal",
+        "source": "https://www.pnas.org/doi/10.1073/pnas.1820843116",
+        "output": "Age (Years)",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "PEDBE.csv",
+            "transform": lambda sum: anti_trafo(sum - 2.1),
+        },
+    },
+    "Zhang_10": {
+        "year": 2019,
+        "species": "Human",
+        "tissue": "Blood",
+        "source": "https://www.nature.com/articles/ncomms14617",
+        "output": "Mortality Risk",
+        "model": {"type": "LinearMethylationModel", "file": "Zhang_10.csv"},
+    },
+    # "DunedinPoAm38": {
+    #     "year": 2020,
+    #     "species": "Human",
+    #     "tissue": "Blood",
+    #     "source": "https://elifesciences.org/articles/54870#s2",
+    #     "output": "Aging Rate (Years/Year)",
+    #     "model": {
+    #         "type": "LinearMethylationModel",
+    #         "file": "DunedinPoAm38.csv",
+    #         "transform": lambda sum: sum - 0.06929805,
+    #     },
+    # },
+    # "DunedinPACE": {
+    #     "year": 2022,
+    #     "species": "Human",
+    #     "tissue": "Blood",
+    #     "source": "https://www.proquest.com/docview/2634411178",
+    #     "output": "Aging Rate (Years/Year)",
+    #     "model": {
+    #         "type": "LinearMethylationModel",
+    #         "file": "DunedinPACE.csv",
+    #         "transform": lambda sum: sum - 1.949859,
+    #         "preprocess": dunedin_pace_normalization,
+    #         "default_imputation": "none",
+    #     },
+    # },
+    "GrimAgeV1": {
+        "year": 2019,
+        "species": "Human",
+        "tissue": "Blood",
+        "source": "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6366976/",
+        "output": "Mortality Adjusted Age (Years)",
+        "model": {"type": "GrimageModel", "file": "GrimAgeV1.csv"},
+    },
+    "GrimAgeV2": {
+        "year": 2022,
+        "species": "Human",
+        "tissue": "Blood",
+        "source": "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9792204/",
+        "output": "Mortality Adjusted Age (Years)",
+        "model": {"type": "GrimageModel", "file": "GrimAgeV2.csv"},
+    },
+    "DNAmTL": {
+        "year": 2019,
+        "species": "Human",
+        "tissue": "Blood, Adipose",
+        "source": "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6738410/",
+        "output": "Telomere Length",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "DNAmTL.csv",
+            "transform": lambda sum: sum - 7.924780053,
+        },
+    },
+    "HRSInCHPhenoAge": {
+        "year": 2022,
+        "species": "Human",
+        "tissue": "Blood",
+        "output": "Age (Years)",
+        "source": "https://www.nature.com/articles/s43587-022-00248-2",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "HRSInCHPhenoAge.csv",
+            "transform": lambda sum: sum + 52.8334080,
+        },
+    },
+    "Knight": {
+        "year": 2016,
+        "species": "Human",
+        "tissue": "Cord Blood",
+        "source": "https://genomebiology.biomedcentral.com/articles/10.1186/s13059-016-1068-z",
+        "output": "Gestational Age",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "Knight.csv",
+            "transform": lambda sum: sum + 41.7,
+        },
+    },
+    "LeeControl": {
+        "year": 2019,
+        "species": "Human",
+        "tissue": "Placenta",
+        "source": "https://www.aging-us.com/article/102049/text",
+        "output": "Gestational Age",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "LeeControl.csv",
+            "transform": lambda sum: sum + 13.06182,
+        },
+    },
+    "LeeRefinedRobust": {
+        "year": 2019,
+        "species": "Human",
+        "tissue": "Placenta",
+        "source": "https://www.aging-us.com/article/102049/text",
+        "output": "Gestational Age",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "LeeRefinedRobust.csv",
+            "transform": lambda sum: sum + 30.74966,
+        },
+    },
+    "LeeRobust": {
+        "year": 2019,
+        "species": "Human",
+        "tissue": "Placenta",
+        "source": "https://www.aging-us.com/article/102049/text",
+        "output": "Gestational Age",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "LeeRobust.csv",
+            "transform": lambda sum: sum + 24.99772,
+        },
+    },
+}
+
+class LinearMethylationModel(PublishedClocksBaseEstimator):
+    def __init__(self, 
+                 name, 
+                 imputation=None, 
+                 transform=None, 
+                 preprocess=None, 
+                ) -> None:
+        self.name = name
+        self.imputation = imputation
+        self.transform = transform
+        self.preprocess = preprocess
+
+        #load the model
+        self.model_meta = model_definitions[name]
+        self.model_params = model_definitions[name]['model']
+        assert self.model_params['type'] == 'LinearMethylationModel', "Model is not Linear Methylation Model!"
+        self.coefficients = pd.read_csv(get_clock_file(self.model_params['file']), index_col=0)
+
+        #use params defined in model metadata if not given in the init
+        if self.imputation is None:
+            self.imputation = self.model_params.get('default_imputation', "sesame_450k")
+            if self.imputation == 'sesame_450k':
+                ivalues = pd.read_csv(get_clock_file("sesame_450k_median.csv"), index_col=0)
+                self.imputation_values = ivalues.loc[self.coefficients.index]
+            
+            #TODO: create a vector of imputation values based on the model sites if sesame is chosen
+
+        if self.transform is None:
+            self.transform =  self.model_params.get("transform", identity)
+            
+        if self.preprocess is None:
+            self.preprocess = self.model_params.get("preprocess", identity)
+
+
+    def predict(self, 
+                X: pd.DataFrame
+                ):
+        intersection = self.coefficients.index.intersection(X.columns)
+        X_ = X[intersection]
+        return X_
+        #X_ = 
+        X_ = self.preprocess(X)
+
+        # Join the coefficients and dnam_data on the index
+        methylation_df = self.coefficients.join(dnam_data, how="inner")
+
+        # Vectorized multiplication: multiply CoefficientTraining with all columns of dnam_data
+        result = (
+            methylation_df.iloc[:, 1:]
+            .multiply(methylation_df["CoefficientTraining"], axis=0)
+            .sum(axis=0)
+        )
+
+        # Return as a DataFrame
+        return result.apply(self.transform).to_frame(name="Predicted")
+
+    def methylation_sites(self):
+        return list(self.coefficients.index)
+
+
+
+
+### TODO: rewrite this class
+class GrimageModel:
+    def __init__(self, coef_file, **metadata):
+        self.coefficients = pd.read_csv(
+            get_clock_file(coef_file), index_col=0
+        )
+        self.metadata = metadata
+
+    @classmethod
+    def from_definition(cls, clock_definition):
+        model_def = clock_definition["model"]
+        return cls(
+            model_def["file"],
+            **{k: v for k, v in clock_definition.items() if k != "model"},
+        )
+
+    def predict(self, geo_data):
+        if "sex" not in geo_data.metadata or "age" not in geo_data.metadata:
+            raise ValueError("Metadata must contain 'sex' and 'age' columns")
+
+        df = geo_data.dnam
+
+        # Transposing metadata so that its structure aligns with dnam (columns as samples)
+        transposed_metadata = geo_data.metadata.transpose()
+
+        # Add metadata rows to dnam DataFrame
+        df.loc["Age"] = transposed_metadata.loc["age"]
+        df.loc["Female"] = transposed_metadata.loc["sex"].apply(
+            lambda x: 1 if x == 1 else 0
+        )
+        df.loc["Intercept"] = 1
+
+        grouped = self.coefficients.groupby("Y.pred")
+        all_data = pd.DataFrame()
+
+        for name, group in grouped:
+            if name == "COX":
+                cox_coefficients = group.set_index("var")["beta"]
+            elif name == "transform":
+                transform = group.set_index("var")["beta"]
+                m_age = transform["m_age"]
+                sd_age = transform["sd_age"]
+                m_cox = transform["m_cox"]
+                sd_cox = transform["sd_cox"]
+            else:
+                sub_clock_result = self.calculate_sub_clock(df, group)
+                all_data[name] = sub_clock_result
+
+        all_data["Age"] = geo_data.metadata["age"]
+        all_data["Female"] = geo_data.metadata["sex"].apply(
+            lambda x: 1 if x == 1 else 0
+        )
+
+        all_data["COX"] = all_data.mul(cox_coefficients).sum(axis=1)
+        age_key = "DNAmGrimAge"
+        accel_key = "AgeAccelGrim"
+        # Calculate DNAmGrimAge
+        Y = (all_data["COX"] - m_cox) / sd_cox
+        all_data[age_key] = (Y * sd_age) + m_age
+
+        # Calculate AgeAccelGrim
+        lm = LinearRegression().fit(
+            all_data[["Age"]].values, all_data[age_key].values
+        )
+        predictions = lm.predict(all_data[["Age"]].values)
+        all_data[accel_key] = all_data[age_key] - predictions
+
+        # Drop COX column after computations
+        all_data.drop("COX", axis=1, inplace=True)
+
+        return all_data
+
+    def calculate_sub_clock(self, df, coefficients):
+        # Filter coefficients for only those present in df
+        relevant_coefficients = coefficients[
+            coefficients["var"].isin(df.index)
+        ]
+
+        # Create a Series from the relevant coefficients, indexed by 'var'
+        coefficients_series = relevant_coefficients.set_index("var")["beta"]
+
+        # Align coefficients with df's rows and multiply, then sum across CpG sites for each sample
+        result = (
+            df.loc[coefficients_series.index]
+            .multiply(coefficients_series, axis=0)
+            .sum()
+        )
+
+        return result
+
+    def rename_columns(self, data, old_names, new_names):
+        for old_name, new_name in zip(old_names, new_names):
+            data.rename(columns={old_name: new_name}, inplace=True)
+
+    def methylation_sites(self):
+        filtered_df = self.coefficients[
+            ~self.coefficients.index.isin(["COX", "transform"])
+        ]
+        unique_vars = set(filtered_df["var"]) - {"Intercept", "Age", "Female"}
+        return list(unique_vars)
--- computage/utils/nan_utils.py
+++ computage/utils/nan_utils.py
@@ -27,3 +27,97 @@ def introduce_nans(X: pd.DataFrame,
     mask = np.random.choice([True, False], size=X_nan.shape, p=[p, 1-p])
     X_nan = X_nan.mask(mask)   
     return X_nan
+
+### IMPUTATION implementation from biolearn: https://github.com/bio-learn/biolearn/blob/master/ ###
+def impute_from_standard(X, cpg_averages, cpgs_to_impute=None):
+    """
+    Impute all missing values in a DNA methylation dataset using the averages from an external dataset.
+
+    Args:
+        dnam (pd.DataFrame): DataFrame with samples as columns and CpG sites as rows.
+        cpg_averages (pd.Series): Series containing reference averages for CpG sites.
+        cpgs_to_impute (list of str, optional): List of CpG sites to impute. Missing cpgs will only be imputed if in this list.
+
+    Returns:
+        pd.DataFrame: DataFrame with missing values filled.
+    """
+    if cpgs_to_impute:
+        impute_rows = X.loc[cpgs_to_impute]
+        impute_rows = impute_rows.apply(lambda col: col.fillna(cpg_averages))
+        df_filled = X.combine_first(impute_rows)
+    else:
+        df_filled = X.apply(lambda col: col.fillna(cpg_averages))
+    return df_filled
+
+
+def impute_from_average(dnam, cpgs_to_impute=None):
+    """
+    Impute all missing values in a DNA methylation dataset using the average from the dataset itself.
+
+    Args:
+        dnam (pd.DataFrame): DataFrame with samples as columns and CpG sites as rows.
+        cpgs_to_impute (list of str, optional): List of CpG sites to impute.
+
+    Returns:
+        pd.DataFrame: DataFrame with missing values filled.
+    """
+    # Protect input from mutation
+    dnam_copy = dnam.copy()
+    means = dnam_copy.mean(axis=1)
+
+    if cpgs_to_impute:
+        # Filter out non-existent CpG sites
+        existing_cpgs = [
+            cpg for cpg in cpgs_to_impute if cpg in dnam_copy.index
+        ]
+
+        # Apply imputation only to existing CpG sites
+        mask = dnam_copy.loc[existing_cpgs].isna()
+        dnam_copy.loc[existing_cpgs] = dnam_copy.loc[existing_cpgs].where(
+            ~mask, means[existing_cpgs], axis=0
+        )
+    else:
+        dnam_copy = dnam_copy.where(dnam_copy.notna(), means, axis=0)
+
+    return dnam_copy
+
+
+def hybrid_impute(dnam, cpg_source, required_cpgs, threshold=0.8):
+    """
+    Imputes missing values in a DNA methylation dataset based on a threshold. 
+    Sites with data below the threshold are replaced from an external source, 
+    while others are imputed using the average of existing values.
+
+    Args:
+        dnam (pd.DataFrame): DataFrame with samples as columns and CpG sites as rows.
+        cpg_source (pd.Series): Series containing reference values for CpG sites.
+        required_cpgs (list of str): List of CpG sites that need to be in the final dataset.
+        threshold (float, optional): Threshold for determining imputation strategy. Default is 0.8.
+
+    Returns:
+        pd.DataFrame: DataFrame with missing values filled.
+
+    Raises:
+        ValueError: If certain required CpG sites are missing from both the dataset and the cpg_source.
+    """
+    # Drop rows below the threshold, these will be replaced entirely from cpg_source
+    cpgs_below_threshold = dnam.notna().mean(axis=1) < threshold
+    dnam = dnam.drop(dnam[cpgs_below_threshold].index)
+
+    # Impute remaining rows using impute_from_average
+    df_filled = impute_from_average(dnam)
+
+    missing_cpgs_from_dataset = set(required_cpgs) - set(df_filled.index)
+    missing_cpgs_from_source = [
+        cpg for cpg in missing_cpgs_from_dataset if cpg not in cpg_source
+    ]
+
+    if missing_cpgs_from_source:
+        raise ValueError(
+            f"Tried to fill the following cpgs but they were missing from cpg_source: {missing_cpgs_from_source}"
+        )
+
+    for cpg in missing_cpgs_from_dataset:
+        df_filled.loc[cpg] = cpg_source.loc[cpg]
+
+    return df_filled.sort_index()
