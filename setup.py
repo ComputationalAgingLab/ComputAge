8060d785ee4a937b817fa3c891be046979aca576
shappiron <fire-paladin@mail.ru>
2025-01-29 17:42:44 +0300

    updated package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='computage',
-    version='0.1.5',
+    version='0.1.6',
     description='A library for full-stack aging clocks design and benchmarking.',
     packages=find_packages(),
     package_data={
